9115e8b7fa26fb620459ecd96adafe343b7621a4
yaniv-walker <yanjiaqi.it@foxmail.com>
2023-02-20 23:01:29 +0800

    Initial commit

---
+++ src/test/java/com/yan/yanrpcframework/YanRpcFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yan.yanrpcframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanRpcFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
